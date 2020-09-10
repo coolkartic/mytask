7f35bf87f429b72fa874aebdf61683c76086e3e2
Vignesh <vignesh@szigony.in>
2020-09-10 16:49:15 +0530

    added package

---
+++ build/precache-manifest.55aebec1e7dbf276ff8f0a4733a5c726.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfda4e98392bb229e9b823d05335189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e302200c8c74a8ed5f64",
+    "url": "/static/css/2.38138cb9.chunk.css"
+  },
+  {
+    "revision": "2441756fb3e3cb23f7b9",
+    "url": "/static/css/main.dbba9497.chunk.css"
+  },
+  {
+    "revision": "e302200c8c74a8ed5f64",
+    "url": "/static/js/2.b2312bfc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b2312bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2441756fb3e3cb23f7b9",
+    "url": "/static/js/main.4cfc9ca8.chunk.js"
+  },
+  {
+    "revision": "f78f0647f78e460952b7",
+    "url": "/static/js/runtime-main.65206410.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/static/media/primeicons.79432094.woff"
+  }
+]);
