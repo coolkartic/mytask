604d1e6e8aeb4b44d64b9d8cb4a4188356fec892
Vignesh <vignesh@szigony.in>
2020-09-15 17:42:47 +0530

    added developer profile

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './header/Header';
 import Task from './task/Task';
+import Footer from './footer/footer';
 
 function App() {
   return (
     <div className="App">
      {/* <Header/> */}
      <Task/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/footer/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+         <div className="text-center"> 
+            <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="vignesh-k-5927b0182"><a class="LI-simple-link" href='https://in.linkedin.com/in/vignesh-k-5927b0182?trk=profile-badge'>Vignesh K</a></div>
+         </div>
+        )
+    }
+}
